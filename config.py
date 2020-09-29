c7b20b3f7d057b91692dcf9232136aef2abff8f3
tomito26 <tommybwah@gmail.com>
2020-09-29 23:18:16 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -4,13 +4,9 @@ class Config:
     General configuration parent class
     '''
     QUOTES_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:tom@localhost/blogsite'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:tom@localhost/blogsite'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-    #simple mde configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -19,7 +15,9 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-  
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 class ProdConfig(Config):
@@ -29,16 +27,22 @@ class ProdConfig(Config):
     Args:
         Config:The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = False
+
+class TestConfig(Config):
     pass
 
 class DevConfig(Config):
     '''
     Development configuration class with general configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     DEBUG = True
 
 
 config_options = {
     'development':DevConfig,
-    'production':ProdConfig
+    'production':ProdConfig,
+    'Test':TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME='ingasiathomas@gmail.com'
 export MAIL_PASSWORD='thom.66as'
 export SECRET_KEY='tom2612'
+export DATABASE_URL='postgresql+psycopg2://moringa:tom@localhost/blogsite'
 
 python3.8 manage.py server
