2476ef81b17f2185f6e981d2c406d555042b9249
tomito26 <tommybwah@gmail.com>
2020-09-29 07:46:27 +0300

    added the secret key to start.sh

--- config.py
+++ config.py
@@ -8,12 +8,18 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+  
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='ingasiathomas@gmail.com'
-eport MAIL_PASSWORD='thom.66as'
-
+export MAIL_PASSWORD='thom.66as'
+export SECRET_KEY='tom2612'
 
 python3.8 manage.py server
