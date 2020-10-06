4627d3ef3b5f95593b5d137a37ee3841198d2344
Thomas Khaemba <tommybwah@gmail.com>
2020-10-06 03:27:23 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MAIL_USERNAME='ingasiathomas@gmail.com'
-export MAIL_PASSWORD='thom.66as'
-export SECRET_KEY='tom2612'
-export DATABASE_URL='postgresql+psycopg2://moringa:tom@localhost/blogsite'
-
-python3.8 manage.py server
